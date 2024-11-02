b13d627bbb86898edff73202da40f9020d98e998
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-11-01 21:22:07 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-# Configuración de la página
 st.set_page_config(page_title='Mi pizarra personalizada', layout='wide', initial_sidebar_state="collapsed")
 
-# Estilos de fuente personalizados
+# Estilos de tipografía
 st.markdown("""
     <style>
     .titulo {
@@ -30,27 +29,37 @@ st.title("Pizarra personalizada")
 
 # Parámetros en barra lateral
 with st.sidebar:
-    st.subheader('Escoge el tamaño del pincel')
+    st.subheader('Escoger el tamaño del pincel')
     stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
     
     st.subheader('Escoge el color del pincel')
-    stroke_color = st.color_picker("Selecciona el color del pincel", '#000000')
-    
-# Fondo de la pizarra
-bg_color = '#d6f0f2'
+    stroke_color = st.radio('Selecciona un color:', 
+                            ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#40E0D0', 
+                             '#0000FF', '#FF00FF', '#800080', '#FFFFFF'], 
+                            index=0,
+                            format_func=lambda x: {
+                                '#FF0000': 'Rojo',
+                                '#FFA500': 'Naranja',
+                                '#FFFF00': 'Amarillo',
+                                '#008000': 'Verde',
+                                '#40E0D0': 'Turquesa',
+                                '#0000FF': 'Azul',
+                                '#FF00FF': 'Magenta',
+                                '#800080': 'Púrpura',
+                                '#FFFFFF': 'Blanco'
+                            }[x])
 
-# Crear el componente de lienzo
+# Parámetros del canvas
+bg_color = '#d6f0f2'
+drawing_mode = "freedraw"
+
+# Componente de canvas
 canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Color de relleno fijo con opacidad
+    fill_color="rgba(255, 165, 0, 0.3)",  # Color de relleno fijo con algo de opacidad
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
     height=500,
     width=600,
-    drawing_mode="freedraw",
     key="canvas",
 )
-
-# Mostrar resultado del lienzo (opcional)
-if canvas_result.image_data is not None:
-    st.image(canvas_result.image_data, caption="Tu dibujo")
